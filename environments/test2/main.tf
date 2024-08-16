02ed6dd8d41492ba4d3b2603dd877acc3dc341ae
Alek Hrycaiko <alekhrycaiko@gmail.com>
2024-08-16 10:14:35 -0700

    update

---
+++ environments/test2/main.tf
@@ -0,0 +1,25 @@
+resource "helm_release" "example" {
+  name       = "my-redis-release"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
+  version    = "6.0.1"
+
+  values = [
+  ]
+
+  set {
+    name  = "cluster.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "metrics.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "service.annotations.prometheus\\.io/port"
+    value = "9127"
+    type  = "string"
+  }
+}
